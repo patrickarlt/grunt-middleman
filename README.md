bbf0853a3503178728156e28d4c8f11cd9e48ee2
Patrick Arlt <patrick.arlt@gmail.com>
2013-12-06 09:31:26 -0800

    first commit

---
+++ README.md
@@ -0,0 +1,148 @@
+# grunt-middleman
+
+> A grunt plugin for running Middleman.
+
+## Getting Started
+This plugin requires Grunt `~0.4.1`
+
+If you haven't used [Grunt](http://gruntjs.com/) before, be sure to check out the [Getting Started](http://gruntjs.com/getting-started) guide, as it explains how to create a [Gruntfile](http://gruntjs.com/sample-gruntfile) as well as install and use Grunt plugins. Once you're familiar with that process, you may install this plugin with this command:
+
+```shell
+npm install grunt-middleman --save-dev
+```
+
+Once the plugin has been installed, it may be enabled inside your Gruntfile with this line of JavaScript:
+
+```js
+grunt.loadNpmTasks('grunt-middleman');
+```
+
+## The "middleman" task
+
+### Overview
+In your project's Gruntfile, add a section named `middleman` to the data object passed into `grunt.initConfig()`.
+
+```js
+grunt.initConfig({
+  middleman: {
+    options: {
+      useBundle: true
+    },
+    server: {},
+    build: {
+      options: {
+        command: "build"
+      }
+    }
+  }
+})
+```
+
+### Options
+
+#### options.command
+Type: `String`
+Default value: `'server'`
+
+This is the command that Middleman will execute. It should be `'server'` or `'build'`. Certain properties can only be used with `'server'` or `'build'`
+
+#### options.useBundle
+Type: `Boolean`
+Default value: `false`
+
+If you are using Bundler set this to true to prefix your command with `bundle exec`.
+
+#### options.environment
+Type: `String`
+Default value: `'development'`
+
+Set the Ruby environment that your command will run in. Generally you should let Middleman handle this.
+
+#### options.host
+Type: `String`
+Default value: `'0.0.0.0'`
+
+Which host middleman will run on. Localhost by default. **Server only**
+
+#### options.post
+Type: `Integer`
+Default value: `4567`
+
+The port the Middleman server will run on. **Server only**
+
+#### options.glob
+Type: `String`
+Default value: `false`
+
+#### options.glob
+Type: `String`
+Default value: `false`
+
+Builds a subset of the Middleman site. **Build only**
+
+#### options.verbose
+Type: `Boolean`
+Default value: `false`
+
+Enable verbose output.
+
+#### options.clean
+Type: `Boolean`
+Default value: `false`
+
+Remove orphaned files from build. **Build only**
+
+#### options.clean
+Type: `Object`
+Default value: `{}`
+
+Additional environment variables to set. This is useful for passing options into your `config.rb` file
+
+
+### Usage Examples
+
+#### Default Options
+In this example the default options will start the Middleman server on port 4567 when you run `grunt middleman`.
+
+```js
+grunt.initConfig({
+  middleman: {
+    options: {
+      command: "server",
+      useBundle: false,
+      environment: "development",
+      host: "0.0.0.0",
+      port: 4567,
+      glob: false,
+      verbose: false,
+      clean: false,
+      env: {}
+    }
+  }
+});
+```
+
+#### Custom Options
+This configuration will use bundler to execute `middleman build` or `middleman server` when you run `grunt middleman:build` or `grunt middleman:server`.
+
+```js
+grunt.initConfig({
+  middleman: {
+    options: {
+      useBundle: true
+    },
+    server: {},
+    build: {
+      options: {
+        command: "build"
+      }
+    }
+  }
+})
+```
+
+## Contributing
+In lieu of a formal styleguide, take care to maintain the existing coding style. Add unit tests for any new or changed functionality. Lint and test your code using [Grunt](http://gruntjs.com/).
+
+## Release History
+
---
+++ tasks/middleman.js
@@ -0,0 +1,113 @@
+/*
+
+ * grunt-middleman
+ * https://github.com/patrickarlt/grunt-middleman
+ *
+ * Copyright (c) 2013 Patrick Arlt
+ * Licensed under the MIT license.
+ */
+
+'use strict';
+
+module.exports = function(grunt) {
+
+  grunt.registerMultiTask('middleman', 'A grunt plugin for running Middleman server.', function() {
+
+    //     [--clean]                  # Remove orphaned files from build (--no-clean to disable)
+    //                                # Default: true
+    // -g, [--glob=GLOB]              # Build a subset of the project
+    //     [--verbose]                # Print debug messages
+    //     [--instrument=INSTRUMENT]  # Print instrument messages
+    //     [--profile]                # Generate profiling report for the build
+
+    // -e, [--environment=ENVIRONMENT]    # The environment Middleman will run under
+    //                                    # Default: development
+    // -h, [--host=HOST]                  # Bind to HOST address
+    //                                    # Default: 0.0.0.0
+    // -p, [--port=PORT]                  # The port Middleman will listen on
+    //                                    # Default: 4567
+    //     [--verbose]                    # Print debug messages
+    //     [--instrument=INSTRUMENT]      # Print instrument messages
+    //     [--disable-watcher]            # Disable the file change and delete watcher process
+    //     [--profile]                    # Generate profiling report for server startup
+    //     [--reload-paths=RELOAD_PATHS]  # Additional paths to auto-reload when files change
+    //     [--force-polling]              # Force file watcher into polling mode
+    // -l, [--latency=N]                  # Set file watcher latency, in seconds
+    //                                    # Default: 0.25
+
+    var options = this.options({
+      command: "server",
+      useBundle: false,
+      environment: "development",
+      host: "0.0.0.0",
+      port: 4567,
+      glob: false,
+      verbose: false,
+      clean: false,
+      env: {}
+    });
+
+    var args = [],
+        done = this.async(),
+        cmd = "middleman";
+
+    // if we are using bundler bundle is the command with exec and middleman as the next args
+    if(options.useBundle){
+      cmd = "bundle";
+      args.push("exec");
+      args.push("middleman");
+    }
+
+    // push the middleman command onto the stack (server or build)
+    args.push(options.command);
+
+    options.server = (options.command === "server");
+
+    // add the verbose option
+    if(options.verbose){
+      args.push("--verbose");
+    }
+
+    // add the glob option (build only)
+    if(!options.server && options.glob){
+      args.push("--glob=" + options.glob);
+    }
+
+    // add the clean option (server only)
+    if(!options.server && options.clean){
+      args.push("--clean");
+    }
+
+    // add the server options
+    if(options.server){
+      args.push("--environment=" + options.environment);
+      args.push("--host=" + options.host);
+      args.push("--port=" + options.port);
+    }
+
+    // copy environment variables
+    for (var variable in options.env) {
+      if(options.env.hasOwnProperty){
+        process.env[variable] = options.env[variable];
+      }
+    }
+
+    // spawn the middleman command
+    grunt.util.spawn({
+      cmd: cmd,
+      args: args,
+      opts: {
+        stdio: "inherit",
+        env: process.env
+      }
+    }, function (error, result, code) {
+      if(error){
+        grunt.log.error("Error running middleman " + options.command + " " + error);
+      } else {
+        grunt.log.ok("Finished running middleman " + options.command);
+      }
+      done();
+    });
+  });
+
+};
